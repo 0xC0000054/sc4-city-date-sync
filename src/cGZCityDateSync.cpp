1e9b96a2e55aa89fcb3ab41e423f87e19e806e2e
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-11-25 23:32:26 -0700

    Add the project

---
+++ src/cGZCityDateSync.cpp
@@ -0,0 +1,423 @@
+////////////////////////////////////////////////////////////////////////
+//
+// This file is part of sc4-city-date-sync, a DLL Plugin for SimCity 4
+// that synchronizes the city dates in a region.
+//
+// Copyright (c) 2023 Nicholas Hayes
+//
+// This file is licensed under terms of the MIT License.
+// See LICENSE.txt for more information.
+//
+////////////////////////////////////////////////////////////////////////
+
+#include "version.h"
+#include "cIGZFrameWork.h"
+#include "cIGZApp.h"
+#include "cIGZCheatCodeManager.h"
+#include "cIGZDate.h"
+#include "cISC4App.h"
+#include "cISC4City.h"
+#include "cISC4Simulator.h"
+#include "cIGZMessageServer2.h"
+#include "cIGZMessageTarget.h"
+#include "cIGZMessageTarget2.h"
+#include "cIGZString.h"
+#include "cRZMessage2COMDirector.h"
+#include "cRZMessage2Standard.h"
+#include "cRZBaseString.h"
+#include "GZServPtrs.h"
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <Windows.h>
+#include "wil/resource.h"
+#include "wil/win32_helpers.h"
+
+static constexpr uint32_t kSC4MessagePostCityInit = 0x26D31EC1;
+static constexpr uint32_t kSC4MessagePreCityShutdown = 0x26D31EC2;
+static constexpr uint32_t kSC4MessagePostRegionInit = 0xCBB5BB45;
+
+static constexpr uint32_t kCityDateSyncPluginDirectorID = 0x9f234be4;
+
+static const uint32_t kGZIID_cISC4App = 0x26ce01c0;
+static constexpr uint32_t kSC4CheatGUIDSimDate = 0x8a78beff;
+
+static constexpr std::string_view PluginLogFileName = "SC4CityDateSync.log";
+
+class cGZCityDateSyncDllDirector : public cRZMessage2COMDirector
+{
+public:
+
+	cGZCityDateSyncDllDirector()
+		: currentSimDate(),
+		  simDateSet(false),
+		  exitingCity(false)
+	{
+		std::filesystem::path dllFolder = GetDllFolderPath();
+
+		std::filesystem::path logFilePath = dllFolder;
+		logFilePath /= PluginLogFileName;
+
+		logFile.open(logFilePath, std::ofstream::out | std::ofstream::trunc);
+		if (logFile)
+		{
+			logFile << "SC4CityDateSync v" PLUGIN_VERSION_STR << std::endl;
+		}
+		cLocale = _create_locale(LC_ALL, "C");
+	}
+
+	uint32_t GetDirectorID() const
+	{
+		return kCityDateSyncPluginDirectorID;
+	}
+
+	bool ShouldChangeCityDate(cIGZDate& cityDate) const
+	{
+		bool result = false;
+
+		const uint32_t cityYear = cityDate.Year();
+
+		if (cityYear < currentSimDate.year)
+		{
+			result = true;
+		}
+		else if (cityYear == currentSimDate.year)
+		{
+			const uint32_t cityMonth = cityDate.Month();
+
+			if (cityMonth < currentSimDate.month)
+			{
+				result = true;
+			}
+			else if (cityMonth == currentSimDate.month)
+			{
+				const uint32_t dayOfMonth = cityDate.DayOfMonth();
+				result = dayOfMonth < currentSimDate.day;
+			}
+		}
+
+		return result;
+	}
+
+	void WriteLogEntry(const char* line)
+	{
+		if (logFile && line)
+		{
+			SYSTEMTIME time;
+			GetSystemTime(&time);
+
+			char buffer[1024]{};
+
+			std::snprintf(buffer,
+				sizeof(buffer),
+				"[%hu:%hu:%hu.%hu] %s",
+				time.wHour,
+				time.wMinute,
+				time.wSecond,
+				time.wMilliseconds,
+				line);
+
+#ifdef _DEBUG
+			PrintLineToDebugOutput(buffer);
+#endif // _DEBUG
+
+			logFile << buffer << std::endl;
+		}
+	}
+
+	void PostCityInit(cIGZMessage2Standard* pStandardMsg)
+	{
+		cISC4City* pCity = reinterpret_cast<cISC4City*>(pStandardMsg->GetIGZUnknown());
+
+		if (pCity)
+		{
+			if (pCity->GetEstablished())
+			{
+				cISC4Simulator* pSimulator = pCity->GetSimulator();
+
+				if (pSimulator)
+				{
+					if (simDateSet)
+					{
+						cIGZDate* cityDate = pSimulator->GetSimDate();
+
+						if (cityDate)
+						{
+							if (ShouldChangeCityDate(*cityDate))
+							{
+								cISC4AppPtr pSc4App;
+
+								if (pSc4App)
+								{
+									cIGZCheatCodeManager* pCheatCodeManager = pSc4App->GetCheatCodeManager();
+
+									if (pCheatCodeManager)
+									{
+										char simDateCommandBuffer[1024]{};
+
+										// The SimDate command supports a single argument value that is formatted in its
+										// internal date representation.
+										// We use the single argument version to work around a bug in the 3 argument
+										// (MM DD YYYY) version.
+										// 
+										// The 3 argument (MM DD YYYY) command sets the calendar date to the next day after
+										// the date you enter, so you have to subtract one day from your selected value when
+										// using it.
+										// For example, to set the simulator date to 1/1/2000 you would have to enter the
+										// command: SimDate 12 31 1999.
+										//
+										// We print the number using the "C" locale, this ensures that the number will
+										// always be printed the same way regardless of the global locale settings that
+										// are in use.
+										int charsWritten = _snprintf_s_l(
+											simDateCommandBuffer,
+											sizeof(simDateCommandBuffer),
+											_TRUNCATE,
+											"SimDate %u",
+											cLocale,
+											currentSimDate.dateNumber);
+
+										if (charsWritten > 0)
+										{
+											// Cache the old city date in local variables for logging purposes.
+											// Because the cityDate variable is a pointer to an internal game structure its
+											// date value will always be the current simulator date at the time its methods
+											// are called, and this isn't what we want.
+											uint32_t oldCityMonth = cityDate->Month();
+											uint32_t oldCityDayOfMonth = cityDate->DayOfMonth();
+											uint32_t oldCityYear = cityDate->Year();
+
+											// SendCheatNotifications always returns false.
+											pCheatCodeManager->SendCheatNotifications(
+												cRZBaseString(simDateCommandBuffer),
+												kSC4CheatGUIDSimDate);
+
+											char logBuffer[1024]{};
+
+											_snprintf_s_l(
+												logBuffer,
+												sizeof(logBuffer),
+												_TRUNCATE,
+												"Changed the city date from %u %u %4u to %u %u %4u.",
+												cLocale,
+												oldCityMonth,
+												oldCityDayOfMonth,
+												oldCityYear,
+												currentSimDate.month,
+												currentSimDate.day,
+												currentSimDate.year);
+
+											WriteLogEntry(logBuffer);
+										}
+										else
+										{
+											WriteLogEntry("Failed to create the SimDate cheat command.");
+										}
+									}
+								}
+							}
+							else
+							{
+								WriteLogEntry("The city has a more recent date than the previous city.");
+							}
+						}
+						simDateSet = false;
+					}
+					else
+					{
+						WriteLogEntry("The city date has not been set for the current region.");
+					}
+				}
+				else
+				{
+					WriteLogEntry("The city has not been established, once it is established the date may be changed the next time it is loaded.");
+				}
+			}
+		}
+	}
+
+	void PreCityShutdown(cIGZMessage2Standard* pStandardMsg)
+	{
+		cISC4City* pCity = reinterpret_cast<cISC4City*>(pStandardMsg->GetIGZUnknown());
+
+		if (pCity)
+		{
+			cISC4Simulator* pSimulator = pCity->GetSimulator();
+
+			if (pSimulator)
+			{
+				cIGZDate* simDate = pSimulator->GetSimDate();
+
+				if (simDate)
+				{
+					currentSimDate.month = simDate->Month();
+					currentSimDate.day = simDate->DayOfMonth();
+					currentSimDate.year = simDate->Year();
+					currentSimDate.dateNumber = simDate->DayNumber();
+
+					simDateSet = true;
+				}
+			}
+		}
+		exitingCity = true;
+	}
+
+	void PostRegionInit()
+	{
+		// We ignore the first PostRegionInit message after exiting a city.
+		// If a second PostRegionInit message is received it indicates that
+		// the player has changed regions.
+		if (exitingCity)
+		{
+			exitingCity = false;
+			return;
+		}
+
+		// The city dates are not synchronized across regions.
+		simDateSet = false;
+	}
+
+	bool DoMessage(cIGZMessage2* pMessage)
+	{
+		cIGZMessage2Standard* pStandardMsg = static_cast<cIGZMessage2Standard*>(pMessage);
+		uint32_t dwType = pMessage->GetType();
+
+		switch (dwType)
+		{
+		case kSC4MessagePostCityInit:
+			PostCityInit(pStandardMsg);
+			break;
+		case kSC4MessagePreCityShutdown:
+			PreCityShutdown(pStandardMsg);
+			break;
+		case kSC4MessagePostRegionInit:
+			PostRegionInit();
+			break;
+		}
+
+		return true;
+	}
+
+	bool PostAppInit()
+	{
+		cIGZFrameWork* const pFramework = RZGetFrameWork();
+		if (!pFramework)
+		{
+			WriteLogEntry("Failed to get the GZCOM framework pointer.");
+			return true;
+		}
+
+		cIGZApp* const pApp = pFramework->Application();
+		if (!pFramework)
+		{
+			WriteLogEntry("Failed to get the GZCOM Application pointer.");
+			return true;
+		}
+
+		cISC4App* pSC4App = nullptr;
+		if (!pApp->QueryInterface(kGZIID_cISC4App, reinterpret_cast<void**>(&pSC4App)))
+		{
+			WriteLogEntry("Failed to get the SC4 Application pointer.");
+			return true;
+		}
+
+		// The SimDate command requires the game's debug mode to be enabled.
+		pSC4App->SetDebugFunctionalityEnabled(true);
+
+		cIGZMessageServer2Ptr pMsgServ;
+		if (pMsgServ)
+		{
+			std::vector<uint32_t> requiredNotifications;
+			requiredNotifications.push_back(kSC4MessagePostCityInit);
+			requiredNotifications.push_back(kSC4MessagePreCityShutdown);
+			requiredNotifications.push_back(kSC4MessagePostRegionInit);
+
+			for (uint32_t messageID : requiredNotifications)
+			{
+				if (!pMsgServ->AddNotification(this, messageID))
+				{
+					WriteLogEntry("Failed to subscribe to the required notifications.");
+					return true;
+				}
+			}
+		}
+		else
+		{
+			WriteLogEntry("Failed to subscribe to the required notifications.");
+		}
+		return true;
+	}
+
+	bool OnStart(cIGZCOM* pCOM)
+	{
+		cIGZFrameWork* const pFramework = RZGetFrameWork();
+
+		if (pFramework->GetState() < cIGZFrameWork::kStatePreAppInit)
+		{
+			pFramework->AddHook(this);
+		}
+		else
+		{
+			PreAppInit();
+		}
+		return true;
+	}
+
+private:
+
+	std::filesystem::path GetDllFolderPath()
+	{
+		wil::unique_cotaskmem_string modulePath = wil::GetModuleFileNameW(wil::GetModuleInstanceHandle());
+
+		std::filesystem::path temp(modulePath.get());
+
+		return temp.parent_path();
+	}
+
+#ifdef _DEBUG
+	void PrintLineToDebugOutputFormatted(const char* format, ...)
+	{
+		char buffer[1024]{};
+
+		va_list args;
+		va_start(args, format);
+
+		std::vsnprintf(buffer, sizeof(buffer), format, args);
+
+		va_end(args);
+
+		PrintLineToDebugOutput(buffer);
+	}
+
+	void PrintLineToDebugOutput(const char* line)
+	{
+		OutputDebugStringA(line);
+		OutputDebugStringA("\n");
+	}
+#endif // _DEBUG
+
+	struct Date
+	{
+		Date() : month(0), day(0), year(0), dateNumber(0)
+		{
+		}
+
+		uint32_t month;
+		uint32_t day;
+		uint32_t year;
+		uint32_t dateNumber;
+	};
+
+	Date currentSimDate;
+	bool simDateSet;
+	bool exitingCity;
+	_locale_t cLocale;
+	std::ofstream logFile;
+};
+
+cRZCOMDllDirector* RZGetCOMDllDirector() {
+	static cGZCityDateSyncDllDirector sDirector;
+	return &sDirector;
+}
---
+++ src/version.h
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////
+//
+// This file is part of sc4-city-date-sync, a DLL Plugin for SimCity 4
+// that synchronizes the city dates in a region.
+//
+// Copyright (c) 2023 Nicholas Hayes
+//
+// This file is licensed under terms of the MIT License.
+// See LICENSE.txt for more information.
+//
+////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#define PLUGIN_VERSION_STR	    "1.0.0"
+#define RESOURCE_VERSION         1,0,0,0
+#define RESOURCE_VERSION_STR    "1.0.0.0"
