79bbd79fac9849a2f9d8c14fd67801a16ec91458
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-01-08 04:14:19 -0700

    Update the copyright year for 2024

--- src/cGZCityDateSync.cpp
+++ src/cGZCityDateSync.cpp
@@ -3,7 +3,7 @@
 // This file is part of sc4-city-date-sync, a DLL Plugin for SimCity 4
 // that synchronizes the city dates in a region.
 //
-// Copyright (c) 2023 Nicholas Hayes
+// Copyright (c) 2023, 2024 Nicholas Hayes
 //
 // This file is licensed under terms of the MIT License.
 // See LICENSE.txt for more information.
